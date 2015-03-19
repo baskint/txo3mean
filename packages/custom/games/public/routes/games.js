4b5226c556facac3f28f70075991ad56c52cfa1d
Baskin Tapkan <baskint@tapkan.com>
2015-03-18 21:43:53 -0500

    games - create angular route added

--- packages/custom/games/public/routes/games.js
+++ packages/custom/games/public/routes/games.js
@@ -2,9 +2,13 @@
 
 angular.module('mean.games').config(['$stateProvider',
   function($stateProvider) {
-    $stateProvider.state('games example page', {
-      url: '/games/example',
-      templateUrl: 'games/views/index.html'
-    });
+    $stateProvider
+        .state('games example page', {
+          url: '/games/example',
+          templateUrl: 'games/views/index.html'
+        }).state('game create', {
+          url: '/games/create',
+          templateUrl: '/games/views/create.html'
+        });
   }
 ]);
